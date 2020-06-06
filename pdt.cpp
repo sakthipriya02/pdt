4484959413c6d45549db64920981d517375b5c10
sakthipriya02 <59326414+sakthipriya02@users.noreply.github.com>
2020-06-06 08:59:52 +0530

    Create pdt.cpp

---
+++ pdt.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,pdt;
+	cout<<"enter two numbers:";
+	cin>>a>>b;
+	pdt=a*b;
+	cout<<"product of"<<a<<"and"<<b<<"is"<<pdt;
+	return 0;
+	
+}
